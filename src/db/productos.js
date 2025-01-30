23d4ad5922b7b2b81c03acb6fb199146ee2d91f1
Ian Franco Fariña Núñez <ianfarina9@gmail.com>
2025-01-30 13:03:52 -0300

    actualizacion de estilos y main.js

--- src/db/productos.js
+++ src/db/productos.js
@@ -1,46 +1,46 @@
 const productos = [
     { 
       id: 1, 
-      nombre: "Iphone 11 Pro Max", 
-      foto: "imgs/apple-iphone-11-pro-max.jpg", 
-      descripcion: "Iphone 11 super pro max", 
+      nombre: "Pollo al Spiedo", 
+      foto: "imgs/pollo-al-spido.webp", 
+      descripcion: "Pollo asado lentamente en un asador vertical, jugoso por dentro y dorado por fuera, con un sabor ahumado y especiado.",
       precio: 222.20
     },
     { 
       id: 2, 
-      nombre: "IPhone 12", 
-      foto: "imgs/apple-iphone-12.jpg", 
-      descripcion: "Iphone 12, flojito", 
+      nombre: "Milanesa Napolitana", 
+      foto: "imgs/milanesa-a-la-napolitana.webp",
+      descripcion: "Milanesa de carne empanizada, cubierta con salsa de tomate, jamón, queso derretido y a veces rodajas de tomate, gratinada al horno.", 
       precio: 333.30
     },
     { 
       id: 3, 
-      nombre: "Camara Canon", 
-      foto: "imgs/camara-canon.jpg", 
-      descripcion: "Camara ultra 4k", 
+      nombre: "Milanesa a Caballo", 
+      foto: "imgs/milanesa_a_caballo.webp", 
+      descripcion: "Milanesa de carne empanizada servida con uno o dos huevos fritos encima, acompañada de papas fritas o ensalada.", 
       precio: 444.40
     },
     { 
       id: 4, 
-      nombre: "Drone DJI", 
-      foto: "imgs/drone-dji.jpg", 
-      descripcion: "Alto drone", 
+      nombre: "Empanadas de Surubí", 
+      foto: "imgs/emp-pescado.webp", 
+      descripcion: " Masa rellena de pescado desmenuzado, cebolla, pimientos y especias, con un toque fresco de limón, horneada o frita.", 
       precio: 555.50
     },
     { 
       id: 5, 
-      nombre: "Auriculares Sony", 
-      foto: "imgs/auriculares-sony.jpg", 
-      descripcion: "Cancelación activa!", 
+      nombre: "Empanada de Carne", 
+      foto: "imgs/emp-carne.webp",
+      descripcion: "Masa rellena de carne picada, cebolla, huevo duro, aceitunas y especias, horneada o frita.",  
       precio: 666.60
     },
     { 
-      id: 5, 
-      nombre: "Auriculares Sony", 
-      foto: "imgs/auriculares-sony.jpg", 
-      descripcion: "Cancelación activa!", 
+      id: 6, 
+      nombre: "Fideos Caseros c/ Salsa", 
+      foto: "imgs/fideos.webp", 
+      descripcion: "Fideos con salsa a elección, bolognesa (con carne molida, tomate, cebolla, zanahoria y especias, cocinada a fuego lento.) o blanca (con manteca, harina, leche y queso, como la clásica salsa bechamel.)", 
       precio: 666.60
-    },
+    }
 ]
 
 export default productos
--- src/main.js
+++ src/main.js
@@ -6,108 +6,6 @@ console.log(productos) // array Productos
 const contenedorProductos = document.getElementById('container-productos')
 console.log(contenedorProductos)
 
-
-const start = () => {
-    console.warn('Se cargó todo el HTML')
-
-    let html = ''
-    //debugger
-    productos.forEach(prod => {
-        console.log(prod)
-        //debugger
-        // html = html + '' === html += ''
-        html += `<div class="card">
-            <article class="card__article">
-                <div class="card__image-container">
-                    <img class="card__image" src="${prod.foto}" alt="${prod.nombre}">
-                </div>
-                <div class="card__content">
-                    <h2 class="card__heading">${prod.nombre}</h2>
-                    <div class="card__description">
-                        <p>${prod.descripcion}</p>
-                    </div>
-                </div>
-            </article>
-        </div>`
-    })
-    // debugger
-    console.log(html) /* Voy a tener varios div.card * 5 -> */
-
-    contenedorProductos.innerHTML = html
-
-}
-
-/*   html += `<div class="card">
-            <article class="card__article">
-                <div class="card__image-container">
-                    <img class="card__image" src="${prod.foto}" alt="${prod.nombre}">
-                </div>
-                <div class="card__content">
-                    <h2 class="card__heading">${prod.nombre}</h2>
-                    <div class="card__description">
-                        <p>${prod.descripcion}</p>
-                    </div>
-                </div>
-            </article>
-        </div>` */
-
-const start2 = () => {
-
-    productos.forEach(prod => {
-        const card = document.createElement('div')
-        card.classList.add('card')
-        //console.log(card)
-        const articulo = document.createElement('article')
-        articulo.classList.add('card__article')
-        //console.log(articulo)
-        card.appendChild(articulo)
-        //console.log(card)
-        const cardImageContainer = document.createElement('div')
-        cardImageContainer.classList.add('card__image-container')
-        const foto = document.createElement('img')
-        foto.classList.add('card__image')
-        foto.src = prod.foto
-        foto.alt = prod.nombre
-        cardImageContainer.appendChild(foto)
-        // console.log(cardImageContainer)
-        articulo.appendChild(cardImageContainer)
-        //console.log(card)
-
-        /* <div class="card__content">
-                    <h2 class="card__heading">${prod.nombre}</h2>
-                    <div class="card__description">
-                        <p>${prod.descripcion}</p>
-                    </div>
-        </div> */
-
-        const cardContent = document.createElement('div')
-        const h2Titulo = document.createElement('h2')
-        h2Titulo.classList.add('card__heading')
-        h2Titulo.textContent = prod.nombre
-        //console.log(h2Titulo)
-        const cardDescripcion = document.createElement('div')
-        cardDescripcion.classList.add('card__description')
-        const parrafo = document.createElement('p')
-        parrafo.textContent = prod.descripcion
-        //console.log(parrafo)
-
-        cardContent.appendChild(h2Titulo)
-        cardDescripcion.appendChild(parrafo)
-        cardContent.appendChild(cardDescripcion)
-        //console.log(cardContent)
-        articulo.appendChild(cardContent)
-        console.log(card)
-
-        contenedorProductos.appendChild(card)
-
-
-    })
-
-
-
-    
-}
-
 const start3 = () => {
 
     const fragmento = document.createDocumentFragment()
@@ -115,7 +13,7 @@ const start3 = () => {
     productos.forEach(prod => {
         const card = document.createElement('div')
         card.classList.add('card')
-        //console.log(card)
+        // console.log(card)
         const articulo = document.createElement('article')
         articulo.classList.add('card__article')
         //console.log(articulo)
@@ -132,12 +30,12 @@ const start3 = () => {
         articulo.appendChild(cardImageContainer)
         //console.log(card)
 
-        /* <div class="card__content">
+        /* <div>
                     <h2 class="card__heading">${prod.nombre}</h2>
                     <div class="card__description">
                         <p>${prod.descripcion}</p>
                     </div>
-        </div> */
+                    </div> */
 
         const cardContent = document.createElement('div')
         const h2Titulo = document.createElement('h2')
@@ -149,14 +47,108 @@ const start3 = () => {
         const parrafo = document.createElement('p')
         parrafo.textContent = prod.descripcion
         //console.log(parrafo)
-
         cardContent.appendChild(h2Titulo)
         cardDescripcion.appendChild(parrafo)
         cardContent.appendChild(cardDescripcion)
-        //console.log(cardContent)
         articulo.appendChild(cardContent)
-        console.log(card)
 
+        // ----------------------------------------------- 
+        //!  FORMULARIO                                     
+        // ----------------------------------------------- 
+
+        const form = document.createElement('form')
+        form.classList.add('card__form')
+        cardContent.appendChild(form)
+        const formContainer = document.createElement('div')
+        formContainer.classList.add('card__form-container')
+        form.appendChild(formContainer)
+
+        const labelBlanca = document.createElement('label')
+        labelBlanca.htmlFor = 'salsaBlanca'
+        labelBlanca.classList.add('card__form-input-container')
+        labelBlanca.classList.add('pasta')
+        labelBlanca.textContent = 'Selecciona la salsa:'
+        formContainer.appendChild(labelBlanca)
+        // console.log(labelBlanca)
+
+        const salsaBlanca = document.createElement('input')
+        salsaBlanca.id = 'salsaBlanca'
+        salsaBlanca.type = 'radio'
+        salsaBlanca.classList.add('card__form-input')
+        salsaBlanca.textContent = 'Salsa Blanca'
+        labelBlanca.appendChild(salsaBlanca)
+        //console.log(formContainer)
+
+        const labelRoja = document.createElement('label')
+        labelRoja.htmlFor = 'salsaBolo'
+        labelRoja.classList.add('card__form-input-container')
+        labelRoja.classList.add('pasta')
+        formContainer.appendChild(labelRoja)
+
+
+
+
+        const labelSg = document.createElement('label')
+        labelSg.htmlFor = 'salsaBlanca'
+        labelSg.classList.add('card__form-input-container')
+        labelSg.classList.add('no-pasta')
+        labelSg.textContent = 'Selecciona la guarnición:'
+        formContainer.appendChild(labelSg)
+
+
+
+
+        const labelPapas = document.createElement('label')
+        labelPapas.htmlFor = 'salsaBlanca'
+        labelPapas.classList.add('card__form-input-container')
+        labelPapas.classList.add('no-pasta')
+        formContainer.appendChild(labelPapas)
+
+
+
+
+        const labelPure = document.createElement('label')
+        labelPure.htmlFor = 'salsaBlanca'
+        labelPure.classList.add('card__form-input-container')
+        labelPure.classList.add('pasta')
+        formContainer.appendChild(labelPure)
+
+
+
+
+        const labelEnsalada = document.createElement('label')
+        labelEnsalada.htmlFor = 'salsaBlanca'
+        labelEnsalada.classList.add('card__form-input-container')
+        labelEnsalada.classList.add('pasta')
+        formContainer.appendChild(labelEnsalada )
+
+
+
+ /*  <form action="" class="card__form">
+      <div class="card__form-container"> 
+       ? <label for="salsaBlanca" class="card__form-input-container pasta" >Selecciona la salsa:
+          *  <input ID="salsaBlanca" type="radio" class="card__form-input"> Salsa Blanca
+          </label>
+       ?   <label for="salsaBolo" class="card__form-input-container pasta">
+          *  <input ID="salsaBolo" type="radio" class="card__form-input"> Salsa Bolognesa
+          </label>
+       ?   <label for="sg" class="card__form-input-container no-pasta" >Selecciona la guarnicion:
+          *  <input ID="sg" type="radio" class="card__form-input"> Sin guar.
+          </label>
+       ?   <label for="papas" class="card__form-input-container no-pasta">
+          *  <input ID="papas" type="radio" class="card__form-input"> Papas fritas
+          </label>
+       ?   <label for="pure" class="card__form-input-container no-pasta">
+          *  <input ID="pure" type="radio" class="card__form-input"> Puré 
+          </label>
+       ?   <label for="ensalada" class="card__form-input-container no-pasta">
+          *  <input ID="ensalada" type="radio" class="card__form-input"> Ensalada
+          </label>
+      </div>
+        </form> */
+
+        console.log(cardContent)
+        //console.log(card)
         fragmento.appendChild(card)
 
 
@@ -168,7 +160,7 @@ const start3 = () => {
     // Intervengo una sola vez el DOM. Evitando la recarga del html
     contenedorProductos.appendChild(fragmento)
 
-    
+
 }
 
 //                          evento        | callback
